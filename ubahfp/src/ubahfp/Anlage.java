90e630805253e18f5cdce4539f0dbe361a0f16ae
Simon Wimmer <simon@smwr.de>
2011-10-20 16:12:48 +0200

    - mit 'a' imd 'd' die attribute der Kläranlagen durchschalten 	-- Ellipse = menge (Vorsicht, nicht relativ zueinander) 	-- Kreuz = boolscher Wert
    - mit SPACE speichert man screenshot im »bin« verzeichniss mit Timestamp
    und name des aktuell angezeigtem attributs

--- ubahfp/src/ubahfp/Anlage.java
+++ ubahfp/src/ubahfp/Anlage.java
@@ -10,19 +10,37 @@ public class Anlage extends Location {
 	private float jahresabwassermenge;
 	private float nAblauf;
 	private float pAblauf;
+	private int pl_n_tech, pl_p_tech, pl_uv, pl_chlorination, pl_ozonation, pl_sand_filtration, pl_micro_filtration, pl_other;
+	private String flussgebietseinheit;
+
+	
+	
+	
+	// Abwassereinigung, pl_n_tech, pl_p_tech, pl_uv, pl_chlorination, pl_ozonation, pl_sand_filtration, pl_micro_filtration, pl_other, pl_other_comment
 	
 	public Anlage(float lat, float lon) {
 		super(lat, lon);
 	}
 	
-	public void setData(String name, String bundesland, int ausbaugroesse, float jahresabwassermenge, float nAblauf, float pAblauf){
+	public void setData(String name, String bundesland, String flussgebietseinheit, int ausbaugroesse, float jahresabwassermenge, float nAblauf, float pAblauf, int pl_n_tech, int pl_p_tech, int pl_uv, int pl_chlorination, int pl_ozonation, int pl_sand_filtration, int pl_micro_filtration, int pl_other ){
 		this.name = name;
 		this.bundesland = bundesland;
 		this.ausbaugroesse = ausbaugroesse;
 		this.jahresabwassermenge = jahresabwassermenge;
 		this.nAblauf = nAblauf;
 		this.pAblauf = pAblauf;
+		this.pl_n_tech = pl_n_tech;
+		this.pl_p_tech = pl_p_tech;
+		this.pl_uv = pl_uv;
+		this.pl_chlorination = pl_chlorination;
+		this.pl_ozonation = pl_ozonation;
+		this.pl_sand_filtration = pl_sand_filtration;
+		this.pl_micro_filtration = pl_micro_filtration;
+		this.pl_other = pl_other;
+		this.flussgebietseinheit = flussgebietseinheit;
+
 	}
+	
 	public String getBundesland() {
 		return bundesland;
 	}
@@ -43,6 +61,42 @@ public class Anlage extends Location {
 	}
 	public float getJahresabwassermenge(){
 		return jahresabwassermenge;
+	}
+	
+	public int get_pl_n_tech(){
+	return pl_n_tech;
+	}	
+	
+	public int get_pl_p_tech(){
+	return	pl_p_tech;
+	}	
+	
+	public int get_pl_uv(){
+	return	pl_uv;
+	}	
+	
+	public int get_pl_chlorination(){
+	return	pl_chlorination;
+	}	
+	
+	public int get_pl_ozonation(){
+	return pl_ozonation;	
+	}	
+	
+	public int get_pl_sand_filtration(){
+	return	pl_sand_filtration;
+	}	
+	
+	public int get_pl_micro_filtration(){
+	return	pl_micro_filtration;
+	}	
+	
+	public int get_pl_other(){
+	return	pl_other;
+	}	
+	
+	public String getFlussgebietseinheit(){
+	return flussgebietseinheit;	
 	}
 
 }
