4ddd8f1b97e0c8737ba7dc46c14be8368a86da92
norrix <norrix@192.168.123.5>
2011-10-20 13:54:29 +0200

    csv loading ausgelagert

---
+++ ubahfp/src/ubahfp/AnlagenLoader.java
@@ -0,0 +1,65 @@
+package ubahfp;
+
+import java.util.Vector;
+
+import processing.core.PApplet;
+
+public class AnlagenLoader {
+	  private Vector<Anlage> positions = new Vector<Anlage>();
+	  
+
+	private PApplet p5;
+	  
+	  public AnlagenLoader(PApplet p){
+		  p5 = p;
+		  
+		  initLoader("pl_dp_join_2008_02_latlong.csv");
+	  }
+	  
+	  public void initLoader(String csvPath){
+		  String lines[] = p5.loadStrings(csvPath);
+		  String [][] csv;
+		  int csvWidth=0;
+		  
+		  //calculate max width of csv file
+		for (int i=0; i < lines.length; i++) {
+		  String [] chars=PApplet.split(lines[i],';');
+		  if (chars.length>csvWidth){
+		    csvWidth=chars.length;
+		  }
+		}
+		
+		//create csv array based on # of rows and columns in csv file
+		csv = new String [lines.length][csvWidth];
+
+		//parse values into 2d array
+		for (int i=0; i < lines.length; i++) {
+		  String [] temp = new String [lines.length];
+		  temp= PApplet.split(lines[i], ';');
+		  for (int j=0; j < temp.length; j++){
+		   csv[i][j]=temp[j];
+		  }
+		}
+
+		
+		// fill positions vector
+		for(int j=1; j < csv.length; j++){
+		   try{
+			   Anlage a = new Anlage(Float.valueOf((csv[j][22]).trim()).floatValue(),Float.valueOf((csv[j][23]).trim()).floatValue());
+			   a.setData(csv[j][0], csv[j][1], Integer.parseInt(csv[j][5]), Float.valueOf((csv[j][6]).trim()).floatValue(), Float.valueOf((csv[j][17]).trim()).floatValue(), Float.valueOf((csv[j][18]).trim()).floatValue());
+		      positions.add(a);
+//		      println(Float.valueOf((csv[j][23]).trim()).floatValue());
+		  }catch (NumberFormatException nfe){
+		    System.out.println("NumberFormatException in row "+(j+1)+" : " + nfe.getMessage());
+		  }	
+		}
+		p5.println(positions.size()+" Anlagen in positions");
+
+		  
+	  }
+	  
+	  public Vector<Anlage> getPositions() {
+			return positions;
+		}
+	  
+}
--- ubahfp/src/ubahfp/Ubahfp.java
+++ ubahfp/src/ubahfp/Ubahfp.java
@@ -12,15 +12,11 @@ public class Ubahfp extends PApplet {
 
 	de.fhpotsdam.unfolding.Map map;
 	
-	  Vector<Anlage> positions = new Vector<Anlage>();
+	private AnlagenLoader al = new AnlagenLoader(this);
+	private Vector<Anlage> positions;
 	
 	public void setup() {
-		
-		
-		// SIMON WAS HERE
-		
-		// AGAIN!
-		
+				
 		  size(800, 600, GLConstants.GLGRAPHICS);
 		  noStroke();
 
@@ -29,45 +25,8 @@ public class Ubahfp extends PApplet {
 		  map.zoomToLevel(6);
 		  map.panTo(new Location(51.5f, 11f));
 		  MapUtils.createDefaultEventDispatcher(this, map);
-
-		  String lines[] = loadStrings("pl_dp_join_2008_02_latlong.csv");
-		  String [][] csv;
-		  int csvWidth=0;
-		  
-		  //calculate max width of csv file
-		for (int i=0; i < lines.length; i++) {
-		  String [] chars=split(lines[i],';');
-		  if (chars.length>csvWidth){
-		    csvWidth=chars.length;
+		  positions = al.getPositions();
 		  }
-		}
-		
-		//create csv array based on # of rows and columns in csv file
-		csv = new String [lines.length][csvWidth];
-
-		//parse values into 2d array
-		for (int i=0; i < lines.length; i++) {
-		  String [] temp = new String [lines.length];
-		  temp= split(lines[i], ';');
-		  for (int j=0; j < temp.length; j++){
-		   csv[i][j]=temp[j];
-		  }
-		}
-
-		
-		// fill positions vector
-		for(int j=1; j < csv.length; j++){
-		   try{
-			   Anlage a = new Anlage(Float.valueOf((csv[j][22]).trim()).floatValue(),Float.valueOf((csv[j][23]).trim()).floatValue());
-			   a.setData(csv[j][0], csv[j][1], Integer.parseInt(csv[j][5]), Float.valueOf((csv[j][6]).trim()).floatValue(), Float.valueOf((csv[j][17]).trim()).floatValue(), Float.valueOf((csv[j][18]).trim()).floatValue());
-		      positions.add(a);
-//		      println(Float.valueOf((csv[j][23]).trim()).floatValue());
-		  }catch (NumberFormatException nfe){
-		    System.out.println("NumberFormatException in row "+(j+1)+" : " + nfe.getMessage());
-		  }	
-		}
-		println(positions.size()+" Anlagen in positions");
-	}
 
 	public void draw() {
 		  background(0);
